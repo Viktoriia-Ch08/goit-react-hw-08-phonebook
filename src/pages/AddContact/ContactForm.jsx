f348710f98e51bdb6b1d1d0b8b05a396938b3895
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-11-24 17:49:15 +0200

    form small fix

--- src/pages/AddContact/ContactForm.jsx
+++ src/pages/AddContact/ContactForm.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts } from 'redux/selectors/selectors';
 import { addContact } from 'redux/operations/contacts.operations';
@@ -11,19 +10,19 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import AddContactButton from 'components/AddContactButton/AddContactButton';
-import { useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   Container,
   Form,
   Input,
   Label,
 } from 'components/ForAllComponents/ForAll.styled';
+import { WarningSpan } from 'components/EditingForm/EditingForm.styled';
 
 const schema = yup
   .object({
     name: yup.string().min(3).max(15).required(),
-    number: yup.string().min(5).max(8).required(),
+    number: yup.string().min(5).max(9).required(),
   })
   .required();
 
@@ -35,34 +34,18 @@ const ContactForm = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+
   const navigate = useNavigate();
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
-  const handleInputChange = event => {
-    switch (event.currentTarget.name) {
-      case 'name':
-        setName(event.currentTarget.value);
-        break;
-      case 'number':
-        setNumber(event.currentTarget.value);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSubmitButton = e => {
-    e.preventDefault();
+  const onSubmit = ({ name, number }) => {
     const data = { name, number };
     contacts.some(element => element.name === data.name)
       ? failedNotification('This contact has already exists')
       : dispatch(addContact(data))
           .unwrap()
           .then(() => {
-            reset();
             navigate('/contacts');
             successfullNotification('You have added the contact!');
           })
@@ -71,36 +54,27 @@ const ContactForm = () => {
           );
   };
 
-  const reset = () => {
-    setName('');
-    setNumber('');
-  };
-
   return (
     <Container>
-      <Form onSubmit={handleSubmitButton}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Label>
           <span className="label-thumb">Name</span>
+          {errors.name && <WarningSpan>{errors.name.message}</WarningSpan>}
           <Input
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleInputChange}
             placeholder="Adam Smith"
-            required
+            {...register('name', {
+              required: true,
+            })}
           />
         </Label>
         <Label>
           <span className="label-thumb">Number</span>
-
+          {errors.number && <WarningSpan>{errors.number.message}</WarningSpan>}
           <Input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={handleInputChange}
             placeholder="233-22-22"
-            maxLength={9}
-            required
+            {...register('number', {
+              required: true,
+            })}
           />
         </Label>
         <AddContactButton />
